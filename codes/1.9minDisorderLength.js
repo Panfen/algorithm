2afdfbb397dcba6011735e2e168c382633960959
panfen <2809956575@qq.com>
2017-08-05 14:03:51 +0800

    'update'

--- codes/1.9minDisorderLength.js
+++ codes/1.9minDisorderLength.js
@@ -3,5 +3,30 @@
 	@remark：
 */
 function minDisorderLength(arr){
-	//
+	if(arr == null || arr.length < 2) return 0;
+
+	var min = arr[arr.length - 1];
+	var noMinIndex = -1;
+	for(var i = arr.length - 2; i != -1; i--){
+		if(arr[i] > min)
+			noMinIndex = i;
+		else
+			min = arr[i];
+	}
+
+	if(noMinIndex == -1) return 0;
+
+	var max = arr[0];
+	var noMaxIndex = -1;
+	for(var i = 1; i != arr.length; i++){
+		if(arr[i] < max)
+			noMaxIndex = i
+		else
+			max = arr[i];
+	}
+
+	return noMaxIndex - noMinIndex + 1;
 }
+
+var arr = [1, 2, 4, 3, 5, 7, 6];
+console.log(minDisorderLength(arr));
